94d68e2ab99b718b78ec8d0b3735420182febba6
Matthew Shedden <70542502+mshedededen@users.noreply.github.com>
2022-05-22 22:05:09 +0100

    Delete Projects/Investment monitoring workflow/Version2 directory

--- Projects/Investment monitoring workflow/Version2/invested_companies code.sql
+++
@@ -1,6 +0,0 @@
-/* CREATE prices_companies TABLE */
-CREATE TABLE prices_companies_V2 (
-    ticker TEXT NOT NULL,
-    price_date DATE NOT NULL,
-    price_close NUMERIC             /* prices can be null, should markets be closed on a given date */
-);
