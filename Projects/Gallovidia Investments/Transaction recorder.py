a5b4ed460f8b5485084c6d621b71bda6fc1a7f48
Matthew Shedden <70542502+mshedededen@users.noreply.github.com>
2023-03-21 22:52:08 +0000

    Upload

---
+++ Projects/Gallovidia Investments/Transaction recorder.py
@@ -0,0 +1,7 @@
+# Transaction recorder
+def record_transactions():
+    print("Launching transaction recorder...")
+    print("Welcome to transaction recorder. The keywords RESET, HELP, and CANCEL can be used at any point.")
+    input("What would you like to do?\n- View transactions: Press V.\n- Record a transaction: Press R\n- Delete a transaction: Press D")
+
+record_transactions
