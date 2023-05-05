ae473416f2249f32c047ca1659adb8974676e805
Matthew Shedden <70542502+mshedededen@users.noreply.github.com>
2023-05-05 20:58:57 +0100

    train work

--- Projects/Gallovidia Investments/Transaction recorder.py
+++ Projects/Gallovidia Investments/Transaction recorder.py
@@ -1,66 +1,90 @@
 import datetime as dt
 
-# Transaction recorder
-def transactions_launcher():
-    print("Launching transactions prompt...")
-    print("Welcome. The keywords RESET, HELP, and CANCEL can be used at any point.")
-    while True:
-        launch_input = input("What would you like to do?\n- View transactions: Press V.\n- Record a transaction: Press R\n- Delete a transaction: Press D")
-        if launch_input == "V" or launch_input == "D":
-            print("Launching a view of all transactions...")
-            break
-            # launch transactions viewer function
-        elif launch_input == "R":
-            print("Launching transaction recorder...")
-            # launch transactions recorder function
-            transactions_recorder()
-            continue
-        elif launch_input == "EXIT":
-            break
-        else:
-            print('Invalid response. Please re-enter.')
-            continue
+class Gallovidia:
+    class TransactionsRecord:
+        """Class to handle the transactions record of Gallovidia Investments.
+        """
+        def __init__(self):
+            self.path = 'C:/Users/PC/OneDrive/GALLOVIDIA INVESTMENTS/Miscellaneous/Transactions/'
+        def recordTransaction(self):
+            """Records a transaction.
+            """
 
-def transactions_recorder():
-    print("Launching transactions recorder...")
-    transactions = dict()
-    print("When prompted, please enter details")
-    # Function to cycle through transactions recorder prompt
-    def transactions_recorder_details(variable, question):
-        while True:
-            recorder_input = input(question)
-            # ---- Overrides -----
-            # Identifier
-            if variable == "Identifier":
-                if len(recorder_input) <= 4: variable = "Ticker"
-                elif len(recorder_input) == 7: variable = "SEDOL"
-                elif len(recorder_input) == 11: variable = "ISIN"
-                else: variable == "Identifier (unknown)"
-            # --------------------
-            confirmation = input("Got it. For {}, adding {}. Are you sure (Y/N)?".format(variable, recorder_input))
-            if confirmation == "Y":
-                transactions.update({variable: recorder_input})
-                break
-            elif confirmation == "N":
-                print("Please try again.")
-                continue
-            elif recorder_input == "":
-                break
-            else:
-                print("Invalid response, please try again.")
-                continue
-    # Dictionary containing questions to ask the user, regarding what information they should fill in
-    prompts_dict = {
-        "Stock name": "What is the name of the stock?",
-        "Identifier": "What is the identifier of the stock (ticker, ISIN, SEDOL)",
-        "Date": "What date was recorded?",
-        "Value": "What is the value transacted?",
-        "Currency_ISO": "What is the currency ISO code? (e.g. GBP, USD, DKK)",
-        }
-    for key, question in prompts_dict.items():
-        transactions_recorder_details(variable=key, question=question)
-    transactions.update({"Recorded": dt.datetime.today()})
-    print(transactions.items())
+            # 1. Check if transaction is a flow (cash transaction) or a security transaction.
+            while True:
+                transaction_type = (input("Is this a cash (flow) transaction or a security transaction? \
+                                          (cash/security): ")
+                                    .lower())
+            # 2. Check further details of transaction
+                if transaction_type == "cash":
+                    print("Processing as cash transaction.")
+                    transaction_subtype = input("Is this a deposit, withdrawal, or investment income (e.g. dividends) ? \
+                                                (deposit/withdrawal/income): ")
+                    break
+                elif transaction_type == "security":
+                    print("Processing as security transaction.")
+                    transaction_subtype = input("Is this a buy or sell? (buy/sell): ")
+                    break
+                else:
+                    print("Invalid input. Please enter 'cash' or 'security'.")
+                    continue
+            
 
-transactions_launcher()
-print("Success.")
+            # 3. Record transaction.
+            # 3.1 Define question sets, in dictionary format
+            # 3.1.1 Cash transaction question set
+            question_set_cash = {
+                "date": "Enter transaction date (YYYY-MM-DD): ",
+                "date_of_entry": dt.datetime.now().strftime("%Y-%m-%d"),
+                "account": "Enter account name: ",
+                "value": "Enter value of transaction: ",
+                "currency_ISO": "Enter the currency ISO: ",
+                "fx_rate": "Enter FX rate (if applicable): ",
+                "fees": "Enter fees (if applicable): "
+                }
+            # 3.1.2 Security transaction question set
+            question_set_security = {
+                "date": "Enter transaction date (YYYY-MM-DD): ",
+                "date_of_entry": dt.datetime.now().strftime("%Y-%m-%d"),
+                "account": "Enter account name: ",
+                "security": "Enter security name: ",
+                "ticker": "Enter ticker symbol (e.g. MSFT for Microsoft): ",
+                "exchange country": "Enter country of exchange: ",
+                "value": "Enter value of transaction: ",
+                "quantity": "Enter quantity: ",
+                "price": "Enter price: (optional)",
+                "currency_ISO": "Enter the currency ISO: ",
+                "fx_rate": "Enter FX rate (if applicable): ",
+                "fees": "Enter fees (if applicable): "
+                }
+            # 3.1.3 Security transaction question set (dividend)
+            question_set_security_dividend = question_set_cash.update(question_set_security)
+
+
+
+
+            # 1.1 Different sets of questions will be available depending on the type of transaction.
+            question_set_cash = ["date", "account", "value", "currency_ISO", "fx_rate", "fees"]
+            question_set_security = ["date", "account", "security", "ticker", "exchange country", "value", "quantity",
+                                     "price", "type", "currency_ISO", "fx_rate", "fees"]
+            
+
+            # 2. Begin recording transaction.
+
+
+
+            # Enter transaction details as a json object.
+            transaction = {
+                "date": input("Enter transaction date (YYYY-MM-DD): "),
+                "account": input("Enter account name: "),
+                "security": input("Enter security name: "),
+                "ticker": input("Enter ticker symbol (e.g. MSFT for Microsoft)"),
+                "exchange country": input("Enter country of exchange"),
+                "value": input("Enter value of transaction: "),
+                "quantity": input("Enter quantity: "),
+                "price": input("Enter price: "),
+                "type": input("Enter transaction type (buy/sell/dividend): "),
+                "currency_ISO": input("Enter the currency ISO"),
+                "fx_rate": input("Enter FX rate (if applicable): "),
+                "fees": input("Enter fees (if applicable): ")
+                }
